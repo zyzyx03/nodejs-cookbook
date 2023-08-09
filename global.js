1872ca1ea2becea4792fa93a24bfad212160d712
zyzyx03 <fauzuwan.nazri@gmail.com>
2023-08-09 22:43:23 +0800

    exploring global object

---
+++ global.js
@@ -0,0 +1,19 @@
+// console.log(global)
+
+// global.setTimeout(() => {
+//   console.log('in the timeout')
+// }, 3000);
+
+// Available by default you dont have to type global
+
+// setTimeout(() => {
+//   console.log('in the timeout')
+//   clearInterval(int)
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('in the interval')
+// }, 1000)
+
+console.log(__dirname)
+console.log(__filename)
