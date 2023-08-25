50185f854c023ec168fa91ba8378e412c442358f
zyzyx03 <fauzuwan.nazri@gmail.com>
2023-08-26 02:39:45 +0800

    add case to route path

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ const server = http.createServer((req ,res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html')
 
+  let path = './views/'
+  switch(req.url){
+    case '/':
+      path += 'index.html'
+      break
+    case '/about':
+     path += 'about.html'
+     break
+    default:
+      path += '404.html'
+  }
+
   // send an html file
-  fs.readFile('./views/index.html', (err, data)=>{
+  fs.readFile('path', (err, data)=>{
     if(err){
       console.log(err)
       res.end
