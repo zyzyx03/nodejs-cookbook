78f7598e61ec5b788aed6d1fbf61d5dec161b2c6
zyzyx03 <fauzuwan.nazri@gmail.com>
2023-08-26 13:47:11 +0800

    ultilizing lodash in server code

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http')
 const fs = require('fs')
+const _ = require('lodash')
 
 const server = http.createServer((req ,res) => {
-  console.log(req.url, req.method)
+  // lodash
+  const num = _.random(0,20)
+  console.log(num)
+
+  const greet = _.once(() => {
+    console.log('Hello')
+  })
+
+  greet()
+  greet()
 
   // set header content type
   res.setHeader('Content-Type', 'text/html')
