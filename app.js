e3bd740e5d21b97ab3e44331dd6990a077c4ec27
zyzyx03 <fauzuwan.nazri@gmail.com>
2023-08-31 01:37:16 +0800

    add static file and pico css

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req,res) => {
-    res.render('index')
+    res.render('index', {title: 'Home'})
+})
+
+
+app.get('/about', (req,res) => {
+  res.render('about', {title: 'About'})
+})
+
+app.get('/blogs/create', (req,res) =>{
+  res.render('create', {title: 'Create'})
+})
+
+app.use((req,res) => {
+  res.status(404).render('404', {title: '404'})
 })
 
 const port = 3000
